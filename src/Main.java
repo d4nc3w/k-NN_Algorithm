719d371331e62cfa415619029910a1f6f330e6e8
s28113 <s28113@pjwstk.edu.pl>
2024-03-18 22:43:54 +0100

    Adding finished k-nn program

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,33 @@ public class Main {
         trainingSet = new ArrayList<>();
     }
 
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        String train_Set = args[1];
+        String test_Set = args[2];
+
+        System.out.print("Do you wish to insert your vector to predict its class? (y/n): ");
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+        if (choice.equalsIgnoreCase("y")) {
+            handleNewVector(k, train_Set);
+
+        } else if (choice.equalsIgnoreCase("n")) {
+            System.out.println("Continuing...");
+            Main main = new Main(k);
+            main.readData(train_Set);
+
+            List<double[]> testVectors = new ArrayList<>();
+            List<String> trueClasses = new ArrayList<>();
+            readTestSet(test_Set, testVectors, trueClasses);
+
+                double accuracy = main.accuracy(testVectors, trueClasses);
+            System.out.println("Accuracy of this model is equal to: " + accuracy + "\n");
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+
     static class Entry {
         double[] attributes;
         String name;
@@ -28,7 +55,6 @@ public class Main {
             this.distance = dist;
             this.name = name;
         }
-
         public double getDistance() {
             return distance;
         }
@@ -37,33 +63,6 @@ public class Main {
             return name;
         }
     }
-
-    public static void main(String[] args) {
-        int k = Integer.parseInt(args[0]);
-        String train_Set = args[1];
-        String test_Set = args[2];
-
-        Main main = new Main(k);
-        main.readData(train_Set);
-        double accuracy = main.accuracy(test_Set);
-        System.out.println("Accuracy of this model is equal to: " + accuracy);
-
-        //Write results for every k from 1 to 100:
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("results.tsv"))) {
-            writer.write("k\taccuracy\n"); // Use tab delimiter
-
-            for (int i = 1; i<= 100; i++) {
-                Main m = new Main(i);
-                m.readData("iris.data");
-                double acc = m.accuracy("iris.test.data");
-                writer.write(k + "\t" + String.format("%.2f", acc) + "\n");
-            }
-
-        } catch(IOException e) {
-            System.out.println("Problem writing to file: " + e.getMessage());
-        }
-    }
-
 
     public static double euclideanDistance(double[] x, double[] y) {
         double sum = 0.0;
@@ -100,7 +99,7 @@ public class Main {
         }
         distances.sort(Comparator.comparingDouble(ProcessedEntry::getDistance));
 
-        for(int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) {
             String name = distances.get(i).getName();
             map.put(name, map.getOrDefault(name, 0) + 1);
         }
@@ -108,8 +107,8 @@ public class Main {
         String prediction = null;
         int max = -1;
 
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            if(entry.getValue() > max) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > max) {
                 max = entry.getValue();
                 prediction = entry.getKey();
             }
@@ -117,11 +116,45 @@ public class Main {
         return prediction;
     }
 
-    public double accuracy(String testSetFile) {
+    public double accuracy(List<double[]> testVectors, List<String> trueClasses) {
         int correct = 0;
-        int total = 0;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(testSetFile));
+        int total = testVectors.size();
+
+        for (int i = 0; i < total; i++) {
+            double[] testVec = testVectors.get(i);
+            String trueClass = trueClasses.get(i);
+            String result = classify(testVec);
+
+            System.out.println("Predicted class: " + result);
+            System.out.println("True class: " + trueClass);
+
+            if (result.equals(trueClass)) {
+                correct++;
+            }
+        }
+        return (double) correct / total;
+    }
+
+    private static void handleNewVector(int k, String train_Set) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the vector (format: x.x,y.y,z.z,w.w): ");
+        String vector = scanner.nextLine();
+
+        String[] vectorParts = vector.split(",");
+        double[] newVector = new double[vectorParts.length];
+
+        for (int i = 0; i < vectorParts.length; i++) {
+            newVector[i] = Double.parseDouble(vectorParts[i]);
+        }
+
+        Main main = new Main(k);
+        main.readData(train_Set);
+        String result = main.classify(newVector);
+        System.out.println("Predicted class: " + result);
+    }
+
+    private static void readTestSet(String test_Set, List<double[]> testVectors, List<String> trueClasses) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(test_Set))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] data = line.split(",");
@@ -129,21 +162,28 @@ public class Main {
                 for (int i = 0; i < testVec.length; i++) {
                     testVec[i] = Double.parseDouble(data[i]);
                 }
-
-                String trueClass = data[data.length - 1];
-                String result = classify(testVec);
-
-               // System.out.println("Predicted class: " + result);
-               // System.out.println("True class: " + trueClass);
-
-                if(result.equals(trueClass)) {
-                    correct++;
-                }
-                total++;
+                testVectors.add(testVec);
+                trueClasses.add(data[data.length - 1]);
             }
         } catch (IOException e) {
-            System.out.println("Problem reading " + testSetFile + " file");
+            System.out.println("Problem reading " + test_Set + " file");
         }
-        return (double) correct / total;
     }
+
+    //I used this method to extract data for every k to draw a graph in Excel (it's no longer valid)
+    /*private static void writeResultsToFile(String trainSet, String testSet) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(trainSet + "_result.tsv"))) {
+            writer.write("k\taccuracy\n");
+
+            for (int i = 1; i <= 100; i++) {
+                Main m = new Main(i);
+                m.readData(trainSet);
+                double acc = m.accuracy(testSet);
+                writer.write(i + "\t" + String.format("%.2f", acc) + "\n");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Problem writing to file: " + e.getMessage());
+        }
+    }*/
 }
